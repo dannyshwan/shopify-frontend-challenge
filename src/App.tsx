032421475ad0b900e67d1e1063a2fc10024540df
Daniel Shwan <dannyshwan@gmail.com>
2022-03-15 10:31:16 -0400

    added date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,135 @@
-import { Layout, Typography, Row, Col, Pagination } from 'antd';
+import { Layout, Typography, Row, Col, Pagination, DatePicker } from 'antd';
 import { SpaceCard } from './components';
 import { Photo } from './types/Photo';
 import Lottie from 'lottie-react';
-import loader from './assets/loading-shapes.json';
+import loader from './assets/loading.json';
 
 import axios from 'axios';
 import { useCallback, useEffect, useState } from 'react';
+import moment from 'moment';
 
 const App = () => {
-	const { Header, Content, Footer } = Layout;
-	const { Text } = Typography;
-	const [images, setImages] = useState<Photo[]>();
-	const [pageSize, setPageSize] = useState<number>(9);
-	const [current, setCurrent] = useState<number>(1);
-	const [minIndex, setMinIndex] = useState<number>(0);
-	const [maxIndex, setMaxIndex] = useState<number>(pageSize);
+  const { Header, Content, Footer } = Layout;
+  const { RangePicker } = DatePicker;
+  const { Text } = Typography;
+  const [images, setImages] = useState<Photo[] | undefined>();
 
-	// Get images from the NASA API
-	const fetchImages = async () => {
-		const res = await axios.get(
-			`${process.env.REACT_APP_API_URL}?api_key=${process.env.REACT_APP_API_KEY}&count=50`
-		);
-		return res.data;
-	};
+  // Pagination
+  const [pageSize, setPageSize] = useState<number>(9);
+  const [current, setCurrent] = useState<number>(1);
+  const [minIndex, setMinIndex] = useState<number>(0);
+  const [maxIndex, setMaxIndex] = useState<number>(pageSize);
 
-	const getImages = useCallback(() => {
-		const fetchImage = async () => {
-			const resp = await fetchImages();
-			setImages(resp);
-		};
+  // Date picker
+  const [startDateState, setStartDateState] = useState<string>(
+    moment().subtract(25, 'days').format('YYYY-MM-DD')
+  );
+  const [endDateState, setEndDateState] = useState<string>(
+    moment().format('YYYY-MM-DD')
+  );
 
-		fetchImage();
-	}, []);
+  // Get images from the NASA API
+  const fetchImages = async (startDate: string, endDate: string) => {
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}?api_key=${process.env.REACT_APP_API_KEY}&start_date=${startDate}&end_date=${endDate}`
+    );
+    return res.data;
+  };
 
-	// Make the call to retrieve images from NASA
-	useEffect(() => {
-		getImages();
-	}, [getImages]);
+  const getImages = useCallback((startDate: string, endDate: string) => {
+    const fetchImage = async () => {
+      const resp: Photo[] = await fetchImages(startDate, endDate);
+      setImages(resp.reverse());
+    };
 
-	const paginationChange = (page: number) => {
-		setCurrent(page);
-		setMinIndex((page - 1) * pageSize);
-		setMaxIndex(page * pageSize);
-	};
+    fetchImage();
+  }, []);
 
-	return (
-		<Layout className="layout">
-			<Header style={{ height: '10vh', display: 'flex', alignItems: 'center' }}>
-				<Text style={{ color: 'white', fontSize: 25 }} strong>
-					Spacestagram 🌙
-				</Text>
-			</Header>
-			<Content
-				style={{
-					padding: '50px 100px',
-					minHeight: '80vh',
-				}}
-			>
-				{images ? (
-					<>
-						<Row>
-							{images?.map(
-								(image, index) =>
-									index >= minIndex &&
-									index < maxIndex && (
-										<Col span={8}>
-											<SpaceCard
-												key={index}
-												imageUrl={image.url}
-												title={image.title}
-												description={image.explanation}
-												date={image.date}
-												author={image.copyright}
-											/>
-										</Col>
-									)
-							)}
-						</Row>
-						<Pagination
-							current={current}
-							pageSize={pageSize}
-							total={images?.length}
-							onChange={paginationChange}
-							onShowSizeChange={(_, size) => setPageSize(size)}
-							style={{ display: 'flex', justifyContent: 'center' }}
-						/>
-					</>
-				) : (
-					// Loading State
-					<Lottie animationData={loader} style={{ height: '60vh' }} loop />
-				)}
-			</Content>
-			<Footer style={{ textAlign: 'center', height: '10vh' }}>
-				©2022 Daniel Shwan - Images from NASA
-			</Footer>
-		</Layout>
-	);
+  // Make the call to retrieve images from NASA
+  useEffect(() => {
+    setImages(undefined);
+    getImages(startDateState, endDateState);
+  }, [startDateState, endDateState]);
+
+  // Action for when we change pages
+  const paginationChange = (page: number) => {
+    setCurrent(page);
+    setMinIndex((page - 1) * pageSize);
+    setMaxIndex(page * pageSize);
+  };
+
+  // Action for when dates are changed
+  const onDateChange = async (_values: any, formatString: [string, string]) => {
+    setStartDateState(formatString[0]);
+    setEndDateState(formatString[1]);
+  };
+
+  // Disable dates past today
+  const disableDates = (current: any) => {
+    return current > moment().endOf('day');
+  };
+
+  return (
+    <Layout className='layout'>
+      <Header style={{ height: '10vh', display: 'flex', alignItems: 'center' }}>
+        <Text style={{ color: 'white', fontSize: 25 }} strong>
+          Spacestagram 🌙
+        </Text>
+      </Header>
+      <Content
+        style={{
+          padding: '50px 100px',
+          minHeight: '80vh',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <RangePicker disabledDate={disableDates} onChange={onDateChange} />
+        {images ? (
+          <>
+            <Row align='top'>
+              {images?.map(
+                (image, index) =>
+                  index >= minIndex &&
+                  index < maxIndex && (
+                    <Col span={8}>
+                      <SpaceCard
+                        key={index}
+                        imageUrl={image.url}
+                        title={image.title}
+                        description={image.explanation}
+                        date={image.date}
+                        author={image.copyright}
+                      />
+                    </Col>
+                  )
+              )}
+            </Row>
+            <Pagination
+              current={current}
+              pageSize={pageSize}
+              total={images?.length}
+              onChange={paginationChange}
+              onShowSizeChange={(_current, size) => setPageSize(size)}
+            />
+          </>
+        ) : (
+          // Loading State
+          <Lottie
+            animationData={loader}
+            style={{ height: '60vh' }}
+            initialSegment={[0, 100]}
+            loop
+          />
+        )}
+      </Content>
+      <Footer style={{ textAlign: 'center', height: '10vh' }}>
+        ©2022 Daniel Shwan - Images from NASA
+      </Footer>
+    </Layout>
+  );
 };
 
 export default App;
--- src/components/SpaceCard.tsx
+++ src/components/SpaceCard.tsx
@@ -5,94 +5,93 @@ import heart from '../assets/colorful-heart.json';
 import { LinkOutlined } from '@ant-design/icons';
 
 interface CardProps {
-	imageUrl: string;
-	title: string;
-	description: string;
-	date: string;
-	author: string;
+  imageUrl: string;
+  title: string;
+  description: string;
+  date: string;
+  author: string;
 }
 
 export const SpaceCard = ({
-	imageUrl,
-	title,
-	description,
-	date,
-	author,
+  imageUrl,
+  title,
+  description,
+  date,
+  author,
 }: CardProps) => {
-	const { Meta } = Card;
-	const { Text } = Typography;
-	const [liked, setLiked] = useState<boolean>(false);
+  const { Meta } = Card;
+  const { Text } = Typography;
+  const [liked, setLiked] = useState<boolean>(false);
 
-	const openNotification = () => {
-		navigator.clipboard.writeText(imageUrl);
-		notification.success({
-			message: 'Image URL has been copied to clipboard',
-			placement: 'bottomRight',
-		});
-	};
+  const openNotification = () => {
+    navigator.clipboard.writeText(imageUrl);
+    notification.success({
+      message: 'Image URL has been copied to clipboard',
+      placement: 'bottomRight',
+    });
+  };
 
-	// Lottie animation options and init
-	const options: LottieOptions = {
-		animationData: heart,
-		autoplay: false,
-	};
+  // Lottie animation options and init
+  const options: LottieOptions = {
+    animationData: heart,
+    autoplay: false,
+  };
 
-	const { View, playSegments } = useLottie(options, { height: '50px' });
+  const { View, playSegments } = useLottie(options, { height: '50px' });
 
-	// Animate the heart if user likes/unlikes photo
-	const likeButtonAction = () => {
-		setLiked(!liked);
-		if (!liked) {
-			playSegments([14, 65], true);
-		} else {
-			playSegments([117, 139], true);
-		}
-	};
+  // Animate the heart if user likes/unlikes photo
+  const likeButtonAction = () => {
+    if (!liked) {
+      playSegments([14, 65], true);
+    } else {
+      playSegments([117, 139], true);
+    }
+    setLiked(!liked);
+  };
 
-	return (
-		<Card
-			hoverable
-			style={{ maxWidth: '25vw', margin: '15px' }}
-			cover={<img alt="example" src={imageUrl} />}
-		>
-			<Meta
-				title={title}
-				description={description}
-				style={{ paddingBottom: '5%' }}
-			/>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					marginBottom: 5,
-				}}
-			>
-				<Text>{author}</Text>
-				<Text>{date}</Text>
-			</div>
-			<div
-				style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
-			>
-				<Button
-					onClick={likeButtonAction}
-					style={{
-						display: 'flex',
-						alignItems: 'center',
-						padding: 0,
-					}}
-					type="text"
-				>
-					{View}
-				</Button>
-				<Button
-					type="text"
-					icon={<LinkOutlined />}
-					size="large"
-					onClick={() => {
-						openNotification();
-					}}
-				/>
-			</div>
-		</Card>
-	);
+  return (
+    <Card
+      hoverable
+      style={{ maxWidth: '25vw', margin: '15px' }}
+      cover={<img alt='example' src={imageUrl} />}
+    >
+      <Meta
+        title={title}
+        description={description}
+        style={{ paddingBottom: '5%' }}
+      />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Text>{author}</Text>
+        <Text>{date}</Text>
+      </div>
+      <div
+        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+      >
+        <Button
+          onClick={likeButtonAction}
+          style={{
+            display: 'inherit',
+            alignItems: 'center',
+            padding: 0,
+          }}
+          type='text'
+        >
+          {View}
+        </Button>
+        <Button
+          type='text'
+          icon={<LinkOutlined />}
+          size='large'
+          onClick={() => {
+            openNotification();
+          }}
+        />
+      </div>
+    </Card>
+  );
 };
